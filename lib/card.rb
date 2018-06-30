fbf0be5ca4c28f62a2087fd99658fdea78dc64b6
Kathleen Yruegas <kathleenecn@gmail.com>
2018-06-30 17:57:19 -0600

    Adds value and suit tests/methods

--- lib/card.rb
+++ lib/card.rb
@@ -1,3 +1,12 @@
 class Card
+  attr_reader :value
+  attr_reader :suit 
+
+  def initialize(value, suit)
+    @value = value
+    @suit = suit
+  end
+
+
 
 end
--- test/card_test.rb
+++ test/card_test.rb
@@ -7,8 +7,18 @@ require 'pry'
 class CardTest < Minitest::Test
 
   def test_does_it_exist
-    card = Card.new
+    card = Card.new("2", "Spades")
     assert_instance_of Card, card
   end
 
+  def test_read_value
+    card = Card.new("2", "Spades")
+    assert_equal "2", card.value
+  end
+
+  def test_suit_reader
+    card = Card.new("2", "Spades")
+    assert_equal "Spades", card.suit
+  end
+  
 end
