62e170f6d1fb6e49a64cb0861717445b7e2aeeeb
Kathleen Yruegas <kathleenecn@gmail.com>
2018-07-02 18:44:05 -0600

    lib/round.rb

--- lib/round.rb
+++ lib/round.rb
@@ -14,9 +14,14 @@ class Round
     response = "#{guess_hash.values[0]} of #{guess_hash.values[1]}"
     guess = Guess.new(response, current_card)
     @guesses << guess
-    guess 
+    guess
   end
 
-
-
+  def number_correct
+    count = 0
+    @guesses.each do |guess|
+      count += 1 if guess.correct?
+    end
+    return count
+  end
 end
