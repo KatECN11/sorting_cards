56ac48685b1fba3304ce16df79c751219c24b179
Kathleen Yruegas <kathleenecn@gmail.com>
2018-07-03 21:07:18 -0600

    finalize sort test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -54,23 +54,8 @@ class DeckTest < Minitest::Test
     assert_equal 4, deck.card_suit_value(deck.cards[3].suit)
   end
 
-  def test_does_it_sort_by_value
-    card_1 = Card.new("4","Hearts")
-    card_2 = Card.new("Jack", "Clubs")
-    card_3 = Card.new("5", "Diamonds")
-    card_4 = Card.new("Ace", "Spades")
-    card_5 = Card.new("Ace", "Diamonds")
-    deck = Deck.new([card_1, card_2, card_3, card_4, card_5])
-
-    sorted_deck = deck.sort_deck(deck)
-
-    actual = sorted_deck.cards
-    expected = [card_1, card_3, card_2, card_4, card_5]
-
-    assert_equal expected, actual
-  end
-
   def test_does_it_sort_by_value_and_suit
+
     card_1 = Card.new("4","Hearts")
     card_2 = Card.new("Jack", "Clubs")
     card_3 = Card.new("5", "Diamonds")
@@ -85,6 +70,4 @@ class DeckTest < Minitest::Test
 
     assert_equal expected, actual
   end
-
-
 end
